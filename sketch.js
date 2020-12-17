74508faf7887842f9f8838900668f90f4a6c60d9
Adyantkumarsingh <70712174+Adyantkumarsingh@users.noreply.github.com>
2020-12-17 15:29:09 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,22 +1,59 @@
-//Create variables here
+var dog,happydog;
+var database;
+var food,foodstack;
 
 function preload()
 {
-	//load images here
+  dog = loadImage('dogImg.png');
+  doghappy = loadImage('dogImg1.png');
 }
 
 function setup() {
 	createCanvas(800, 700);
-  
+  dog = createSprite(300,350,10,10);
+
+  foodstack = database.ref('food');
+  foodstack.on("value",readStock);
+
 }
 
 
 function draw() {  
+
+  background(46,139,87);
+
+  if(keyWentDown(UP_ARROW)){
+    writeStock(food);
+    dog.addImage(dogHappy);
+  }
+
+  Text("Press up arrow key to feed tommy Milk",100,100);
+  textSize(150);
+  Fill(0);
+  Stroke(255,0,0);
+
+
+
 
   drawSprites();
   //add styles here
 
 }
 
+  function readStock(data){
+    foods = data.val();
+  }
+
+  function writeStock(x){
+
+    if(x<= 0){
+      x = 0;
+    }else{
+      x = x-1;
+    }
+    databasee.ref('/').update({
+      food:x
+    })
+  }
 
 
